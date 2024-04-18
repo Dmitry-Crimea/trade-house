9cb178cd734f8caace6c883a868d4549759c7ff8
Брыкин Дмитрий Валериевич <dbrykin@dev.vtb.ru>
2024-04-18 14:25:35 +0300

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-wcwqze
+from aiogram import Bot, Dispatcher
+import asyncio
+
+bot = Bot(token='')
+dp = Dispatcher()
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__'
