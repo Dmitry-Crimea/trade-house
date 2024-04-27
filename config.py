5acca27c59ef4e15e379185fb55a1ab2ded49eff
Брыкин Дмитрий Валериевич <dbrykin@dev.vtb.ru>
2024-04-27 14:02:32 +0300

    Добавил локальную базу

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os, sys
 #     sys.exit(1)
 
 ECHO=True
-ENGINE='https://storage.yandexcloud.net/db-sqlite/db.sqlite3?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=YCAJElphcDdOukjTmGPvxJEuQ%2F20240427%2Fru-central1%2Fs3%2Faws4_request&X-Amz-Date=20240427T104536Z&X-Amz-Expires=864000&X-Amz-Signature=7E80E36F3526F92D1D0EB77D35033B4D6F2724EC72C3F24F29556D7237E07A1A&X-Amz-SignedHeaders=host'
+ENGINE='sqlite+aiosqlite:///db.sqlite3'
 TOKEN='6862227221:AAFz0PpN0X6mZIvxY1-lv3zBofPGq_Nzv-8'
 # ENGINE=os.getenv("BOT_ENGINE")
 # TOKEN = os.getenv("BOT_TOKEN")
